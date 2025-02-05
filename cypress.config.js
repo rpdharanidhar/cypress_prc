d46073cec489d795b0b91b271c09e2db042f5d16
Dharanidhar Rangasamy Pushparaj <dpushparaj@cloudvice.com>
2025-02-05 17:26:44 +0530

    final v1

--- cypress.config.js
+++ cypress.config.js
@@ -8,35 +8,34 @@
 //     setupNodeEvents(on, config) {
 //       // implement node event listeners here
 //     },
-//     downloadsFolder: "cypress/downloads", // Custom downloads folder      
+//     downloadsFolder: "cypress/downloads", // Custom downloads folder
 //   },
 // });
 
 const { defineConfig } = require("cypress");
- 
+
 const fs = require("fs");
 const path = require("path");
- 
- 
- 
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-        on("after:screenshot", (details) => {
-          const randomFileName = `screenshot_${Date.now()}.png`;
-          const newPath = path.join(path.dirname(details.path), randomFileName);
-          return new Promise((resolve, reject) => {
-            fs.rename(details.path, newPath, (err) => {
-              if (err) {
-                console.error("Error renaming screenshot:", err);
-                return reject(err);
-              }
-              console.log(`Screenshot renamed to: ${randomFileName}`);
-              resolve({ path: newPath });
-            });
+      on("after:screenshot", (details) => {
+        const randomFileName = `screenshot_${Date.now()}.png`;
+        const newPath = path.join(path.dirname(details.path), randomFileName);
+        return new Promise((resolve, reject) => {
+          fs.rename(details.path, newPath, (err) => {
+            if (err) {
+              console.error("Error renaming screenshot:", err);
+              return reject(err);
+            }
+            console.log(`Screenshot renamed to: ${randomFileName}`);
+            resolve({ path: newPath });
           });
         });
+      });
     },
+    downloadsFolder: "cypress/downloads", // Custom downloads folder
     defaultCommandTimeout: 1000000,
     requestTimeout: 1000000,
     // experimentalSessionAndOrigin: true0,
